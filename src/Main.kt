fb5cb24245dc32bcc5ab7032788b15d902a0c8f2
RuVMakarov <RuVMakarov@IMM-14-509-1M>
2025-10-01 08:44:06 +0300

    Initial commit

---
+++ src/Main.kt
@@ -0,0 +1,67 @@
+import com.sun.source.util.Plugin
+import kotlin.math.*
+
+class Polynome {
+    val coeffs: DoubleArray
+    val degree: Int
+
+    constructor(coeffs: DoubleArray) {
+        this.coeffs = coeffs
+        this.degree = this.coeffs.size - 1
+    }
+
+    constructor() : this(doubleArrayOf(0.0))
+    constructor(i: Int) : this(DoubleArray(i, { 0.0 }))
+
+
+    override fun toString(): String {
+        var str = ""
+        for (i in this.degree downTo 0) {
+            if (coeffs[i] == 0.0) continue
+            str += { c: Double -> if (c > 0) " + " else " - " }(coeffs[i])
+            if (str == " + ") str = ""
+            str += "${abs(coeffs[i])}"
+            if (i != 0) str += "*x^$i"
+        }
+        return str
+    }
+
+    operator fun plus(other: Polynome): Polynome {
+        var my_degree = max(this.degree, other.degree)
+        var my_coeffs: DoubleArray = DoubleArray(my_degree + 1, { 0.0 })
+        for (i in 0..my_degree) {
+            val c = { c: DoubleArray, d: Int -> if (d >= i) c[i] else 0.0 }
+            val x = c(this.coeffs, this.degree)
+            val y = c(other.coeffs, other.degree)
+            my_coeffs[i] += x + y
+        }
+        return Polynome(my_coeffs)
+    }
+
+    operator fun times(k: Double):Polynome{
+        return Polynome(DoubleArray(this.degree+1, {i:Int->coeffs[i]*k}))
+    }
+
+    operator fun times(k: Int) = this.times(k*1.0)
+    //operator fun Int.times(p: Polynome) = p.times(this)
+
+    operator fun times(other:Polynome):Polynome{
+        var my_coeffs = DoubleArray(this.degree+other.degree+1, {0.0})
+        for(i in 0..this.degree)
+            for( j in 0..other.degree)
+                my_coeffs[i+j] += this.coeffs[i]*other.coeffs[j]
+        return Polynome(my_coeffs)
+    }
+}
+
+operator fun Double.times(p: Polynome) = p.times(this)
+
+
+
+fun main() {
+    var a = Polynome(doubleArrayOf(1.0, 2.0))
+    var b = Polynome(doubleArrayOf(1.0, 1.0))
+    //var c = a + b
+    //println(c.degree)
+    println(a)
+}
